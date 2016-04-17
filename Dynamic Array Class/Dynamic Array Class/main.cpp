aa6946f4eb645ec067a54526a4299e7279f65317
Sergi Parra i Ramírez <egrupoparra@gmail.com>
2016-04-17 03:01:30 +0200

    Uploading test main.cpp
    Main to test the dynamic array class

--- Dynamic Array Class/Dynamic Array Class/main.cpp
+++ Dynamic Array Class/Dynamic Array Class/main.cpp
@@ -1,11 +1,35 @@
-#include <stdlib.h>
+#include "dynarray.h"
+#include <iostream>
+#include <stdio.h>
+
 using namespace std;
 
 int main()
 {
-	int* marray = new int[5];
-	marray = (int*) realloc(marray, sizeof(marray) + sizeof(int) * 2);
-	for (int i = 0; i < 7;i++)
-		*marray++ = 1;
-	delete[] marray;
+	darray<int> myarray;
+
+	printf("Original Size: %d\nOriginal Capacity: %d\n", myarray.size(), myarray.capacity());
+
+	myarray.push_back(0);
+	myarray.push_back(1);
+	myarray.push_back(2);
+	myarray.push_back(3);
+	myarray.push_back(4);
+
+	myarray.insert(5, 5, 12);
+
+	for (int i = 0; i < myarray.size(); i++)
+	{
+		printf("%d\n", myarray[i]);
+	}
+
+	printf("Size: %d\nCapacity: %d\nEmptying...", myarray.size(), myarray.capacity());
+
+	while (!myarray.empty())
+		myarray.pop_back();
+
+	printf("\nSize: %d\nCapacity: %d\n", myarray.size(), myarray.capacity());
+
+	getchar();
+	return 0;
 }
