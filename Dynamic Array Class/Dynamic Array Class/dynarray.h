e03d70e654be66c3a1051f16869e7b9c2aceb2b2
Sergi Parra i Ramírez <egrupoparra@gmail.com>
2016-04-17 03:01:09 +0200

    Uploading finished DynArray class
    Changelog
    Properly implemented already done constructors.
    Implemented constructor by copy
    Implemented w/r access with [] operator
    Implemented push_back
    Implemented pop_back
    Implemented insert
    Implemented remove

--- Dynamic Array Class/Dynamic Array Class/dynarray.h
+++ Dynamic Array Class/Dynamic Array Class/dynarray.h
@@ -3,54 +3,145 @@
 
 #include <stdlib.h>
 #define DYNARRAY_BLOCK_SIZE 16
+#define darray DynArray
 
 template <class T> class DynArray
 {
 private:
-	T* buffer;
-	unsigned int capacity;
-	unsigned int size;
+	T* _buffer;
+	unsigned int _capacity;
+	unsigned int _size;
 
 public:
-	DynArray::DynArray():
-		capacity (DYNARRAY_BLOCK_SIZE),
-		size(0)
+	DynArray():
+		_capacity(DYNARRAY_BLOCK_SIZE),
+		_size(0)
 	{
-		buffer = (T*)malloc(sizeof(T) * capacity);
+		_buffer = (T*)malloc(sizeof(T) * _capacity);
 	}
 
-	DynArray::DynArray(const unsigned int _size, const T _value = NULL)
+	DynArray(const unsigned int& size, const T& value = 0)
 	{
-		capacity = DYNARRAY_BLOCK_SIZE * (_size / DYNARRAY_BLOCK_SIZE + 1);
-		buffer = (T*)malloc(sizeof(T)* capacity);
+		_capacity = DYNARRAY_BLOCK_SIZE * (size / DYNARRAY_BLOCK_SIZE + 1);
+		_buffer = (T*)malloc(sizeof(T) * _capacity);
+		_size = size;
 
-		size = _size;
-		for (int i = 0; i < size; ++i)
-			buffer* = _value;
-	}
+		T* buffer = _buffer;
+		for (int i = 0; i < _size; ++i)
+			*buffer = value;
+	} //this constructor and the default one could be merged with DynArray(const unsigned int& size = 0, const T& value = 0)
 
-	DynArray::DynArray(const DynArray<T>& reference)
+	DynArray(const DynArray<T>& reference)
 	{
-		capacity = reference.capacity;
-		buffer = (T*)malloc(sizeof(T)* capacity);
+		_capacity = reference.capacity();
+		_buffer = (T*)malloc(sizeof(T)* _capacity);
+		_size = reference.size();
 
-		size = reference.size;
-		for (int i = 0; i < size; ++i)
+		T* buffer = _buffer;
+		for (int i = 0; i < _size; ++i)
 		{
-			T* _buffer = buffer;
-			_buffer++ = reference.buffer++;
+			*buffer = reference[i];
+			buffer++;
 		}
 	}
 
-	DynArray::~DynArray()
+	~DynArray()
 	{
-		free(buffer);
-		buffer = nullptr;
+		free(_buffer);
+		_buffer = nullptr;
+	}
+
+	T& operator[] (const unsigned int& pos) const
+	{
+		T* buffer = _buffer + pos;
+
+		return *buffer;
+	}
+
+	T& operator[] (const unsigned int& pos)
+	{
+		T* buffer = _buffer;
+		buffer += pos;
+
+		return *buffer;
+	}
+
+	unsigned int size() const
+	{
+		return _size;
+	}
+
+	unsigned int capacity() const
+	{
+		return _capacity;
+	}
+
+	bool empty()
+	{
+		return _size == 0;
+	}
+
+	void push_back(const T& item)
+	{
+		if (_size >= _capacity)
+		{
+			_buffer = (T*)realloc(_buffer, (_capacity + DYNARRAY_BLOCK_SIZE)*sizeof(T));
+			_capacity += DYNARRAY_BLOCK_SIZE;
+		}
+
+		T* buffer = _buffer + _size++;
+		*buffer = item;
+	}
+
+	void pop_back()
+	{
+		--_size;
+	}
+
+	void insert(const unsigned int& position, const T& value, const unsigned int& nelements = 1)
+	{
+		_size += nelements;
+		if (_size > _capacity)
+		{
+			_buffer = (T*)realloc(_buffer, (_capacity + DYNARRAY_BLOCK_SIZE)*sizeof(T));
+			_capacity += DYNARRAY_BLOCK_SIZE;
+		}
+
+		T* buffer = _buffer + _size - nelements;
+		T* buffer_end = _buffer + _size;
+
+		for (int i = 0; i <= nelements; ++i)
+		{
+			*buffer_end = *buffer;
+			++buffer;
+			++buffer_end;
+		}
+
+		for (int i = 0; i <= nelements; ++i)
+		{
+			--buffer;
+			*buffer = value;
+		}
+		
+	}
+
+	void remove(const unsigned int& first, unsigned int last = 0)
+	{
+		if (last < first)
+			last = first;
+		T* buffer = _buffer + first;
+		T* buffer_end = _buffer + last + 1;
+
+		for (int i = 0; i < (_size -1) - last; ++i)
+		{
+			*buffer = *buffer_end;
+			++buffer;
+			++buffer_end;
+		}
+
+		_size -= last - first + 1;
 	}
 
 };
 
-
-
-
-#endif
+#endif //__DYNARRAY_H__
